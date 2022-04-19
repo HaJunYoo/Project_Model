ee1a3b2a92f243fd437e06240f2b9a865b48719e
Ha Jun Yoo <58240677+HaJunYoo@users.noreply.github.com>
2022-04-19 16:37:55 +0900

    update

--- BigProject2/settings.py
+++ BigProject2/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eofxj_6j%pd_)bgjn3=3i_w3z9_kz#qk-vr(-^q%)1h8(^sq)%'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
